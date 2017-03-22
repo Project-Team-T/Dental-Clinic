940c11180dc116db089c0cfc154e9ef514ad33a1
Xiangting Fan <isfanxt@gmail.com>
2017-03-22 12:18:30 +0000

    dentist

--- Server_End/Dental_Clinic/src/main/java/com/dentalclinic/dentist/DentistController.java
+++ Server_End/Dental_Clinic/src/main/java/com/dentalclinic/dentist/DentistController.java
@@ -126,9 +126,9 @@ public class DentistController {
 		}
 	
 	@GetMapping(path="/querytimeavailabe")
-	public @ResponseBody List<DaysAvailableWithDentist> queryavailabletime (@RequestParam int dentist_id){
+	public @ResponseBody List<DaysAvailableWithDentist> queryavailabletime (@RequestParam int dentistId){
 		List<DaysAvailableWithDentist> schedules = new ArrayList<>();
-		schedules.addAll(this.scReposity.findTimeAvailableByDentistId(dentist_id));
+		schedules.addAll(this.scReposity.findTimeAvailableByDentistId(dentistId));
 		return schedules;	
 	}
 	
